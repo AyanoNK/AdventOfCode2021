33f77253231b3064788ba611eb740cf6adbeb074
Ayano <s.correa.wilches@gmail.com>
2021-12-12 09:18:00 +0700

    Solved day 1

---
+++ day_1.py
@@ -0,0 +1,30 @@
+from main import get_question_data
+
+response = get_question_data(year=2021, day=1)
+
+parsed_data = response.splitlines()
+
+parsed_data = [int(x) for x in parsed_data]
+
+
+bigger_previous = []
+ankle = None
+for data in parsed_data:
+    if ankle and data > ankle:
+        bigger_previous.append(ankle)
+    ankle = data
+
+print('pt1', len(bigger_previous))
+
+bigger_previous = []
+for ranges in range(0, len(parsed_data)):
+    try:
+        calc = parsed_data[ranges] + \
+            parsed_data[ranges + 1] + parsed_data[ranges + 2]
+        if ankle and calc > ankle:
+            bigger_previous.append(ankle)
+    except IndexError:
+        pass
+    ankle = calc
+
+print('pt2', len(bigger_previous))
