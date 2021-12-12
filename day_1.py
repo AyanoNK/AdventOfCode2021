4594056a8bf35c106fe7682a0bdcfa447b94ca88
Ayano <s.correa.wilches@gmail.com>
2021-12-12 09:23:21 +0700

    Updated imports

--- day_1.py
+++ day_1.py
@@ -1,4 +1,4 @@
-from imports.data_import import get_question_data
+from data_import import get_question_data
 
 response = get_question_data(year=2021, day=1)
 
--- day_2.py
+++ day_2.py
@@ -1,4 +1,5 @@
-from imports.data_import import get_question_data
+from data_import import get_question_data
+
 
 response = get_question_data(year=2021, day=2)
 
