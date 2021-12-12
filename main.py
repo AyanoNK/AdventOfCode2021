ace09754b9e9b55c4948f2fc976861033a3d84e9
Ayano <s.correa.wilches@gmail.com>
2021-12-12 09:17:54 +0700

    Main commit and data recollection

---
+++ main.py
@@ -0,0 +1,20 @@
+import requests
+
+
+cookies = {
+    'session': '53616c7465645f5f199dc392a3e7c6f5b3988259e6f458710d127077b7fe7f71d615259591b9c76bb1c43f28a3db37ef'
+}
+
+year = 2021
+day = 1
+
+
+def get_question_data(year, day):
+    url = 'http://adventofcode.com/{year}/day/{day}/input'.format(
+        year=year, day=day)
+
+    request = requests.get(url, cookies=cookies, headers={
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.93 Safari/537.36'})
+
+    response = request.text
+    return response
