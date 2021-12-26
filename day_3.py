66c9c56e54699f2ce89c9482266baf3ef5d07e04
Ayano <s.correa.wilches@gmail.com>
2021-12-26 12:03:56 +0700

    Initial day 3

--- day_3.py
+++ day_3.py
@@ -4,4 +4,15 @@ response = get_question_data(year=2021, day=3)
 
 parsed_data = response.splitlines()
 
-print(parsed_data)
+splitted = []
+for binary in parsed_data:
+    splitted.append([n for n in binary])
+
+transposed = []
+trans = ''
+
+
+x = [item[0] for item in splitted]
+y = list(list(zip(splitted)))
+print(parsed_data[:1])
+print(y[:1])
